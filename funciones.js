42e4483ae06c37b49b164447a1db3ab09d02f9ac
renatocuellar <matealva@gmail.com>
2021-01-16 21:20:57 -0500

    Añadimos las explicaciones de variables y funciones

---
+++ funciones.js
@@ -0,0 +1,23 @@
+// HAY DOS TIPOS DE FUNCIONES
+
+// Funciones Declarativas
+
+function miFuncion(){
+    return 3;
+}
+
+miFuncion();
+
+//Funciones de Expresión
+
+var miFuncion = function (a, b){
+    return a + b; 
+}
+
+miFuncion();
+
+
+function saludaEstudiantes(estudiante){
+    console.log("Hola "Estudiante " Cómo amaneces el día de hoy");
+}
+

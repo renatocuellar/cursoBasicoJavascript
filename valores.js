d0367d376cc7c78a46f172d532b3c78a7d47ae10
renatocuellar <matealva@gmail.com>
2021-01-16 19:34:42 -0500

    Añadimos los primeros archivos de JS

---
+++ valores.js
@@ -0,0 +1,32 @@
+*/ Tipos de valores */
+
+*/ESTOS SON LOS VALORES PRIMITIVOS */
+
+*/ número */
+
+40
+
+*/ Strings para texto */
+
+"Renato cuéllar"
+
+*/ booleano */
+
+true
+false
+
+*/valores vacios*/
+
+null */ Cuando aún no hay un valor, para después poder añadir un valor*/
+undefined
+
+
+*/ESTOS SON LOS VALORES TIPO OBJETO*/
+
+*/ valor tipo arrail*/
+
+[1,2,3]
+
+*/ Valor tipo objeto */
+
+{ Nombre: "Diego"}

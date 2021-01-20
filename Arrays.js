1fd751395c7d97fc45f2ccfead90e085973ee052
renatocuellar <matealva@gmail.com>
2021-01-19 22:32:39 -0500

    Terminamos arrays

--- Arrays.js
+++ Arrays.js
@@ -72,3 +72,104 @@ var borrarFruta = fruta.shift("Uvas");
 var posicion = frutas.indexOf("Fresa");
 
 console.log(posicion);
+
+// Este ejemplo es imaginando que recibimos una lista de 30 carros diferentes y se nos solicita que se almacene esta información, 
+// como estamos aprendiendo a usar js por que es un lenguaje cool y no queremos tipiar todo de manera normal y aburrida, usaremos 
+// este lenguaje c: así hacemos un pequeño trabajo de separar todas las marcas, modelos y años respectivamente en distintos arrays 
+// para así lograr juntarlos en uno solo usando una función constructiva la cual indicara cada auto ingresado con su marca, modelo 
+// y año respectivamente.
+
+var marca = ["Audi", "Subaru", "Lexus", "Porsche", "BMW", "Mazda", "Buick", "Toyota", "Kia", "Honda", "Hyundai", "Volvo", "Mini", "Mercedes-Benz", "Volkswagen", "Ford", "Lincoln", "Scion", "Acura", "Chevrolet", "Nissan", "Infiniti", "GMC", "Cadillac", "Dodge", "Land", "Rover", "Mitsubishi", "Jeep", "Fiat"];
+var modelo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+var annio = [2020,2019,2018,2020,2020,2020,2018,2018,2020,2020,2020,2018,2018,2020,2020,2019,2020,2020,2019,2019,2020,2020,2019,2019,2019,2020,2019,2019,2018,2020];
+var listaAutos = [];
+function autoN(marca, modelo, annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+} 
+for (var i = 0; i<30; i++){   
+    var autoNuevo = new autoN(marca[i],modelo[i],annio[i]);
+    listaAutos.push(autoNuevo);
+}
+
+// Luego se nos solicito que filtremos los carros por medio del año actual, ya que deseaban saber la cantidad de autos nuevos 
+// ingresados. Gracias a nuestro trabajo esto se podía saber de la siguiente manera:
+
+var autosFiltrados = listaAutos.filter(function(auto){
+    return auto.annio === 2020;
+});
+console.log(autosFiltrados);
+
+// Así logramos obtener una lista con todos los autos del año 2020 c:
+
+// Posterior a eso, se necesitaba obtener una lista urgentemente de las marcas de los 30 autos que acababan de ingresar por 
+// razones ajenas que no nos interesan c: Obviando que nosotros ya tenemos esta lista creada xD y queremos usar js para esto, 
+// esta tarea es tan sencilla como hacer: 
+
+var marcasRecientes = listaAutos.map(function(auto){
+    return auto.marca;
+});
+console.log(marcasRecientes);
+
+// Así obtenemos nuestra lista de marcas recientemente ingresadas c:
+
+/* A modo de recopilación:
+
+find() : Devuelve el primer elemento del array que cumpla con la condición dada
+foreach() : Ejecuta lo que le definamos una vez por cada elemento de nuestro array
+some() : Comprueba si al menos un elemento del array cumple con la condición que le damos
+filter() : Devuelve todos los elementos del array que cumplan con la condición dada
+Acá te dejo la documentación de cada uno: find() - foreach() - some() - filter()  */
+
+var articulos = [
+    { nombre: "Bici", costo: 3000 },
+    { nombre: "TV", costo: 2500 },
+    { nombre: "Libro", costo: 320 },
+    { nombre: "Celular", costo: 10000 },
+    { nombre: "Laptop", costo: 20000 },
+    { nombre: "Teclado", costo: 500 },
+    { nombre: "Audifonos", costo: 1700 },
+];
+
+//filter Genera un nuevo array
+var articulosFiltrados = articulos.filter(function(articulo){
+    return articulo.costo <= 500; //articulos con precio menor a 500 pesos
+});
+
+//map Ayuda a mapear ciertos elementos de los articulos, es necesario generar nuevo array
+var nombreArticulos = articulos.map(function(articulo){
+    return articulo.nombre;
+});
+
+//find Ayuda a encontrar algo dentro del array articulos
+var encuentraArticulo = articulos.find(function(articulo){
+    return articulo.nombre === "Laptop";
+});
+
+//forEach No es necesario generar nuevo array, se utiliza para realizar un recorrido de un array principal
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+});
+
+//some Se genera nuevo array, regresa un condición en Boolean
+var articulosBaratos = articulos.some(function(articulo){
+    return articulo.costo <= 700;
+});
+
+
+// splice y shift sirven para eliminar objetos
+
+var frutas = ["pera", "banana", "manzana", "trigo"]
+//posicion  	0 		1		 2		 3
+
+frutas.splice(pos,nº de elementos por eliminar)// metodo
+
+//EJM:
+frutas.splice(2,1)
+frutas // [“pera”, “banana”, “trigo”]
+
+//EJM 2: mismo array
+frutas.splice(2,2)
+frutas // [“manzana”, “trigo”]
+>
